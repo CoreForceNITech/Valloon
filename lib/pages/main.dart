0da2f06359b58cda63ababcc84abf44d6f843af5
schezo817 <95132440+schezo817@users.noreply.github.com>
2022-05-10 13:49:15 +0900

    ver0.0.8.2　エラーの修正

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -10,6 +10,7 @@ import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:valloon/In_Out/In.dart';
 
